b7b9dc9af0fd855f3da5caf18ea22725ea66ebe1
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-11-19 16:06:34 -0500

    update pg scripts

--- model.py
+++ model.py
@@ -2,18 +2,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class pg_model(nn.module):
+class pg_model(nn.Module):
     def __init__(self):
         super(pg_model, self).__init__()
         self.l1 = nn.Linear(4, 10)
-        self.l2 = nn.Linear(2, 2)
+        self.l2 = nn.Linear(10, 2)
         self.l3 = nn.Linear(2, 2)
 
-    def forward(x):
+    def forward(self, x):
         x = self.l1(x)
         x = F.relu(x)
         x = self.l2(x)
         x = F.relu(x)
         x = self.l3(x)
-        #x = F.softmax(x)
+        x = F.softmax(x, dim=1)
         return x
--- policy_gradient.py
+++ policy_gradient.py
@@ -1,41 +1,70 @@
 import gym
 import model
 import torch
-
-net = model.pg_model()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.8)
-loss_fn = torch.nn.CrossEntropyLoss()
-NUM_EPSIODES = 1000
-
-env = gym.make('CartPole-v0')
-for i in NUM_EPSIODES:
-    env.reset()
-    not_done = False
-    env.reset()
-    state_holder = []
-    reward_holder = []
-    observation = np.array([0, 0, 0, 0])
-    while not_done:
-        env.render()
-        action = net(observation)
-        state_holder.append(observation)
-        observation, reward, not_done, info = env.step(simulate_action(action))
-        reward_holder.append(reward)
-    dr = discount_and_normalize(reward_list, gamma=.9)
-    loss_fn()
+import torch.nn as nn
+import torch.nn.functional as F
+import wandb
+import os
 
 
 def simulate_action(action_prob):
-    action_take = torch.bernoulli(action_prob[0])
+    # print(action_prob)
+    action_take = torch.bernoulli(action_prob[0][0])
     return action_take
 
 
-def discount_and_normalize(reward_list, gamma=.9):
-    discounted_reward_list = np.zeros(len(reward_list))
+def discount_and_normalize(reward_list, gamma=.99):
+    discounted_reward_list = torch.zeros(len(reward_list))
     cumulative = 0
-    for i in reversed(rande(len(reward_list))):
+    for i in reversed(range(len(reward_list))):
         cumulative = cumulative * gamma + reward_list[i]
         discounted_reward_list[i] = cumulative
     discounted_reward_list = (
-        discounted_reward_list-np.mean(discounted_reward_list))/np.std(discounted_reward_list)
+        discounted_reward_list-torch.mean(discounted_reward_list))/torch.std(discounted_reward_list)
     return discounted_reward_list
+
+
+net = model.pg_model()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+#loss_fn = torch.nn.CrossEntropyLoss(reduce=False)
+NUM_EPSIODES = 10000
+SAVE_TIME = 10
+#writer = SummaryWriter()
+env = gym.make('CartPole-v1')
+wandb.init()
+wandb.hook_torch(net)
+for i in range(NUM_EPSIODES):
+    env.reset()
+    done = False
+    state_holder = []
+    reward_holder = []
+    action_holder = []
+    observation = torch.tensor([0, 0, 0, 0]).float()
+    w = 0
+    k = 0
+    while not done:
+        env.render(mode='rgb_array')
+        # print(observation)
+        action = net(observation.unsqueeze(0))
+        state_holder.append(observation)
+        act = torch.distributions.categorical.Categorical(action).sample()
+        # torch.tensor([1, 0] if act else [0, 1])
+        action_holder.append(act.item())
+        observation, reward, done, info = env.step(action_holder[-1])
+        observation = torch.tensor(observation).float()
+        reward_holder.append(reward)
+        k += 1
+    if w % 100:
+        torch.save(net, os.path.join(wandb.run.dir, "model.pt"))
+    wandb.log({'cumm_reward': sum(reward_holder), 'eps_len': k})
+    dr = discount_and_normalize(reward_holder, gamma=.9)
+    state_batch_run = torch.stack(state_holder)
+    action_batch = torch.tensor(action_holder)
+    y_pred = net(state_batch_run)
+    res = torch.distributions.categorical.Categorical(
+        y_pred).log_prob(action_batch)
+    optimizer.zero_grad()
+    final_loss = torch.sum(-res*dr)
+    final_loss.backward()
+    optimizer.step()
+    w += 1
